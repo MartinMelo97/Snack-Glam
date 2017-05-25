3c2a6d7a52bf7277606cf47048e31aaf096dfcce
MartinMelo97 <nitram-210397@hotmail.com>
2017-05-25 17:10:32 -0500

    Animations

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,11 +1,30 @@
 $('document').ready(function(){
      $(".button-collapse").sideNav();
+     $('.parallax').parallax();
     // $('.logo_nav').css({'margin-top':'50vh'});
     var paddingtop = $('#elnav').height();
     console.log(typeof(paddingtop));
     $('.video-containerr').css({'padding-top':paddingtop.toString()+" px"});
     console.log(paddingtop.toString());
     console.log($('.video-containerr').css('padding-top'));
+
+    var freshDiv = $('#Delicious');
+    var freshDivoffset = freshDiv.offset().top/2;
+
+    var bolicakesDiv = $('#Bolicakes');
+    var bolicakesDivoffset = bolicakesDiv.offset().top - 300;
+    
+    $(document).on('scroll', function(){
+        if($(document).scrollTop() > freshDivoffset && !freshDiv.hasClass('animated'))
+        {
+            $('#Delicious .textote').addClass('animated bounceInLeft').css({'display':'block'});
+        }
+
+        if($(document).scrollTop() > bolicakesDivoffset && !bolicakesDiv.hasClass('animated'))
+        {
+            $('#Bolicakes .textote').addClass('animated bounceInRight').css({'display':'block'});
+        }
+    });
 });
 
 var video = $('#video-background').get(0);
